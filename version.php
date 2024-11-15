91d9fc22ad66ff33e1a0659f3c07f0866ec353ae
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2024-11-15 16:53:59 +1100

    issue #909: bump version

--- version.php
+++ version.php
@@ -25,11 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024101000;
-$plugin->release = 2024101000;
-$plugin->requires = 2022112800;    // Our lowest supported Moodle (3.3.0).
+$plugin->version = 2024111500;
+$plugin->release = 2024111500;
+$plugin->requires = 2024100700;    // Our lowest supported Moodle (4.5.0).
 $plugin->supported = [405, 405];
-// TODO $plugin->incompatible = ;  // Available as of Moodle 3.9.0 or later.
 $plugin->component = 'tool_dataflows';
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [];
